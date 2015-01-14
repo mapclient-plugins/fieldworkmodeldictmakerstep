600a96fda6eaff987c6843788724e734a3f93c71
juzhang <jzha263@aucklanduni.ac.nz>
2015-01-15 11:05:45 +1300

    updated to be python3 compliant, __stepname__ and __location__ added

--- mapclientplugins/fieldworkmodeldictmakerstep/__init__.py
+++ mapclientplugins/fieldworkmodeldictmakerstep/__init__.py
@@ -19,5 +19,8 @@ This file is part of MAP Client. (http://launchpad.net/mapclient)
 """
 __version__ = '0.1.0'
 __author__ = 'Ju Zhang'
+__stepname__ = 'Fieldwork Model Dict Maker'
+__location__ = 'https://github.com/mapclient-plugins/fieldworkmodeldictmakerstep/archive/master.zip'
+
 from mapclientplugins.fieldworkmodeldictmakerstep import step
 import mapclientplugins.fieldworkmodeldictmakerstep.resources_rc
